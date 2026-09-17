0c5456d279ff0c6ce2836e9eba62b92b1ea2af36
Tobi Abidoye <tobiabidoye14@gmail.com>
2026-09-17 11:27:53 -0400

    added some more tests

--- kv/replication_test.go
+++ kv/replication_test.go
@@ -23,9 +23,46 @@ func TestReplicationKv(t *testing.T) {
 	t.Log("item put in kv store testing if puts actually work over network")
 	val, _, err := myClerk.Get("apple")
 
-	if val == "" || err == kvrpc.ErrNoKey {
+	if val == "" || err == kvrpc.ErrNoKey || val != "orange" {
 		t.Fatal("item not able to be put in the key value store")
 	}
 
 	t.Logf("item replicated successfully %s:", val)
 }
+
+func TestOverWriteKv(t *testing.T) {
+	baseDirs := []string{}
+	for i := range 3 {
+		baseDir := t.TempDir()
+		baseDirs = append(baseDirs, baseDir)
+		StartTestKvServer(t, i, baseDir)
+	}
+
+	ports := util.DynamicPorts(3)
+	myClerk := MakeClerk(ports)
+	myClerk.Put("apple", "orange", 0)
+
+	val, _, err := myClerk.Get("apple")
+
+	if val == "" || err == kvrpc.ErrNoKey || val != "orange" {
+		t.Fatal("item not able to be put in the key value store")
+	}
+
+	myClerk.Put("apple", "banana", 0)
+	val, _, err = myClerk.Get("apple")
+
+	if val == "" || err == kvrpc.ErrNoKey || val != "orange" {
+		t.Fatal("false update in kv store")
+	}
+
+	myClerk.Put("apple", "banana", 1)
+	val, _, err = myClerk.Get("apple")
+
+	if val == "" || err == kvrpc.ErrNoKey || val != "banana" {
+		t.Fatal("update to key not successful")
+	}
+
+	t.Log("overwrite logic is shown to be good")
+}
+
+
