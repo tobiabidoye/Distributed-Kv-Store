bd9e61053bb8eeb76f7d0750ec5f08dcc7e0be79
Tobi Abidoye <tobiabidoye14@gmail.com>
2026-09-25 09:53:44 -0400

    finished defining sequential model

--- kv/linearizability_test.go
+++ kv/linearizability_test.go
@@ -1,7 +1,7 @@
 package kv
 
 import (
-	"structs"
+	"maps"
 
 	"github.com/anishathalye/porcupine"
 )
@@ -36,5 +36,50 @@ var kvModel = porcupine.Model{
 				Version uint64
 			}),
 		}
+	},
+	Step: func(state, input, output any) (bool, any) {
+		st := state.(KvState)
+		inp := input.(KvInput)
+		out := output.(KvOutput)
+
+		next := KvState{
+			Data: make(map[string]struct {
+				Value   string
+				Version uint64
+			}),
+		}
+
+		maps.Copy(next.Data, st.Data)
+		entry, exists := next.Data[inp.Key]
+
+		switch inp.Op {
+		//what get result is supposed to be and state after a get
+		case "Get":
+			if !exists {
+				return out.Err == "ErrNoKey", next
+			}
+			match := (out.Err == "OK" && out.Value == entry.Value && out.Version == entry.Version)
+			return match, next
+		case "Put":
+			currentVer := entry.Version
+			if !exists {
+				currentVer = 0
+			}
+
+			if inp.Version == currentVer {
+				if out.Err != "OK" {
+					return false, next
+				}
+
+				next.Data[inp.Key] = struct {
+					Value   string
+					Version uint64
+				}{Value: inp.Value, Version: currentVer + 1}
+				return true, next
+			} else {
+				return out.Err == "ErrVersion", next
+			}
+		}
+		return false, next
 	},
 }
